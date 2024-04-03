498dfbaa92061a990a0c44c552d8e38a86a5904d
oscaremilio <oscaremilio_@hotmail.es>
2024-04-03 22:43:24 +0200

    Usando el método "save()"

--- 2b-intro-mongoose/ejercicio-1-libros/index.js
+++ 2b-intro-mongoose/ejercicio-1-libros/index.js
@@ -3,3 +3,38 @@ const Libro = require(__dirname + "/models/libro");
 
 mongoose.connect("mongodb://127.0.0.1:27017/libros");
 
+/*
+let libro1 = new Libro({
+    titulo: "El capitán Alatriste",
+    editorial: "Alfaguara",
+    precio: 15
+});
+libro1.save().then(
+    resultado => {
+        console.log("Libro añadido:", resultado)
+}).catch(
+    error => {
+        console.log("ERROR añadiendo contacto:", error)
+});
+
+let libro2 = new Libro({
+    titulo: "El juego de Ender",
+    editorial: "Ediciones B",
+    precio: 8.95
+});
+
+libro1.save().then(
+    resultado => {
+        console.log("Libro añadido:", resultado)
+}).catch(
+    error => {
+        console.log("ERROR añadiendo contacto:", error)
+});
+libro2.save().then(
+    resultado => {
+        console.log("Libro añadido:", resultado)
+}).catch(
+    error => {
+        console.log("ERROR añadiendo contacto:", error)
+});
+*/
