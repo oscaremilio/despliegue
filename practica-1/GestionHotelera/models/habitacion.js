29c94b98e0224e52270991dca43af9220ec56378
oscaremilio <oscaremilio_@hotmail.es>
2024-04-16 18:02:15 +0200

    Comentarios añadidos al código de la parte 1 del proyecto.

--- practica-1/GestionHotelera/models/habitacion.js
+++ practica-1/GestionHotelera/models/habitacion.js
@@ -1,5 +1,7 @@
+// Añade la librería necesaria
 const mongoose = require("mongoose");
 
+// Esquema para registrar las incidencias que se produzcan en la habitación
 let incidenciaSchema = new mongoose.Schema({
     descripcion: {
         type: String,
@@ -15,6 +17,7 @@ let incidenciaSchema = new mongoose.Schema({
     }
 });
 
+// Esquema y modelo para la colección de habitaciones del hotel
 let habitacionSchema = new mongoose.Schema({
     numero: {
         type: Number,
@@ -39,9 +42,12 @@ let habitacionSchema = new mongoose.Schema({
         required: true,
         min: 0,
         max: 250
-    },
+    }, // Subdocumento del modelo habitaciones
     incidencias: [incidenciaSchema]
 });
 
+// Guarda el esquema en el modelo y crea la colección 
 let Habitacion = mongoose.model("habitaciones", habitacionSchema);
+
+// Exporta el contenido para el resto de archivos del proyecto
 module.exports = Habitacion;
--- practica-1/GestionHotelera/models/limpieza.js
+++ practica-1/GestionHotelera/models/limpieza.js
@@ -1,7 +1,9 @@
+// Añade la librería necesaria
 const mongoose = require("mongoose");
 
+// Esquema con la información de las limpieza realizada en la habitación
 let limpiezaSchema = new mongoose.Schema({
-    idHabitacion: {
+    idHabitacion: { // Informa del id de la habitación correspondiente
         type: mongoose.Schema.Types.ObjectId,
         ref: "habitaciones"
     },
@@ -15,5 +17,8 @@ let limpiezaSchema = new mongoose.Schema({
     }
 });
 
+// Esquema y modelo para la colección de limpiezas de las habitaciones
 let Limpieza = mongoose.model("limpiezas", limpiezaSchema);
+
+// Exporta el contenido para el resto de archivos del proyecto
 module.exports = Limpieza;
