67c6c860f1ca525a5abbe035168c60c0fc14b94b
oscaremilio <oscaremilio_@hotmail.es>
2024-04-30 18:21:06 +0200

    Falta terminar varios servicios en "routes/limpiezas.js"

--- practica-1/GestionHotelera/index.js
+++ practica-1/GestionHotelera/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/test-hotel');
+mongoose.connect('mongodb://127.0.0.1:27017/hotel');
 
 // Enrutadores
 const habitaciones = require(__dirname + "/routes/habitaciones.js");
--- practica-1/GestionHotelera/routes/limpiezas.js
+++ practica-1/GestionHotelera/routes/limpiezas.js
@@ -1,1 +1,34 @@
+/* 
+Fichero que contiene el enrutador para las URIs 
+que comiencen por "/limpiezas"
+*/
+
+// Añade las librerías necesarias
+const express = require("express");
+
+const router = express.Router();
+
+// Incorpora los modelos de datos
+const Limpieza = require(__dirname + "./models/limpieza.js");
+
+// Obtener limpiezas de una habitación
+router.get("/limpiezas/:id", (req, res) => {
+    Limpieza.find()
+    .then(resultado => {
+            res.status(200).send({ok:true, resultado: resultado});
+    }).catch(error => {
+        res.status(500).send({ok: false, error: "No hay limpiezas registradas para esa habitación"});
+    });
+});
+
+// Obtener el estado de limpieza actual de una habitación
+router.get("/limpiezas/:id/estadolimpieza", (req, res) => {
+    // TODO:
+});
+
+// Actualizar limpieza
+router.post("/limpiezas/:id", (req, res) => {
+    // TODO:
+});
+
 module.exports = router;
