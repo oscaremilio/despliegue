739272e1d26a6afbc3d8e2bcf688f3cdb68501d0
oscaremilio <oscaremilio_@hotmail.es>
2024-05-09 18:32:23 +0200

    Creado el archivo auth.js que gestiona la autenticación.

---
+++ practica-1/GestionHotelera/auth/auth.js
@@ -0,0 +1,40 @@
+// Archivo que gestiona la autenticación basada en tokens
+
+// Librería necesaria
+const jwt = require('jsonwebtoken');
+
+// Palabra secreta que cifra el contenido del token
+const secreto = "secretoNode";
+
+// Función que genera un token dados un login y rol válidos del usuario
+let generarToken = login => jwt.sign({ login: login, rol: rol }, secreto, { expiresIn: "2 hours" })
+
+// Función que valida el token que se reciba
+let validarToken = token => {
+    try {
+        let resultado = jwt.verify(token, secreto);
+        return resultado;
+    } catch (e) { }
+}
+
+// Función que protege una ruta si se tiene el token válido
+let protegerRuta = rol => {
+    return (req, res, next) => {
+        let token = req.headers['authorization'];
+        if (token) {
+            token = token.substring(7);
+            let resultado = validarToken(token);
+            if (resultado && (rol === "" || rol === resultado.rol))
+                next();
+            else
+                res.send({ ok: false, error: "Usuario no autorizado" });
+        } else
+            res.send({ ok: false, error: "Usuario no autorizado" });
+    }
+};
+
+module.exports = {
+    generarToken: generarToken,
+    validarToken: validarToken,
+    protegerRuta: protegerRuta
+};
